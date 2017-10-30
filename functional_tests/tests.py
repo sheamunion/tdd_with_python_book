31351da289aa0b07b1090e6018077500aa5af668
Shea Munion <sheamunion@gmail.com>
2017-10-30 12:23:21 -0500

    Add new URL, view, and template to display lists.

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -71,7 +71,7 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertNotIn('make a fly', page_text)
 
-        inputbox = self.browser.find_by_element_id('id_new_item')
+        inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Buy milk')
         inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy milk')
